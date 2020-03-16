1d80cf8316a5febe52e4b6546bc17079fa86be9d
ccrum292 <ccrum292@gmail.com>
2020-03-16 19:24:07 -0400

    begain server.js

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const   aaa_22 = 44
-function    name() {
-lsdjfkasdjf
-ljkkjlkj___ljsdakf
-}
+const express = require("express");
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+var db = require("./models");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+
+
+
+
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+});
