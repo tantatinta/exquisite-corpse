5797f1773e7f0b3da90a7e424e2cc989285b8f95
ccrum292 <ccrum292@gmail.com>
2020-03-17 20:24:19 -0400

    small changes

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -37,26 +37,28 @@ module.exports = function(app) {
         var lastSentence = [];
         var idOfLastSentence = [];
         entryData.forEach(function(val){
-          if(val.dataValues.Entries.length<3){
-            var allText;
-            var last;
-            var storyId;
-            if(val.dataValues.Entries.length>1){
-              allText = val.dataValues.Entries[val.dataValues.Entries.length-1].dataValues.text;
-              storyId = val.dataValues.Entries[val.dataValues.Entries.length-1].dataValues.StoryId;
-              var splitText = allText.match(/\(?[^\.\?\!]+[\.!\?]\)?/g);
-              if(splitText.length>1){
-                last = splitText[splitText.length - 1];
+          if(lastSentence.length<10){
+            if(val.dataValues.Entries.length<3){
+              var allText;
+              var last;
+              var storyId;
+              if(val.dataValues.Entries.length>1){
+                allText = val.dataValues.Entries[val.dataValues.Entries.length-1].dataValues.text;
+                storyId = val.dataValues.Entries[val.dataValues.Entries.length-1].dataValues.StoryId;
+                var splitText = allText.match(/\(?[^\.\?\!]+[\.!\?]\)?/g);
+                if(splitText.length>1){
+                  last = splitText[splitText.length - 1];
+                }else{
+                  last = splitText[0];
+                }
+                lastSentence.push(last);
+                idOfLastSentence.push(storyId);
               }else{
-                last = splitText[0];
+                allText = val.dataValues.Entries[0].dataValues.text;
+                storyId = val.dataValues.Entries[0].dataValues.StoryId;
+                lastSentence.push(allText);
+                idOfLastSentence.push(storyId);
               }
-              lastSentence.push(last);
-              idOfLastSentence.push(storyId);
-            }else{
-              allText = val.dataValues.Entries[0].dataValues.text;
-              storyId = val.dataValues.Entries[0].dataValues.StoryId;
-              lastSentence.push(allText);
-              idOfLastSentence.push(storyId);
             }
           }
         });
