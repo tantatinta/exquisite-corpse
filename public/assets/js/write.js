bd81d8db14c76cdc484aefc11142fe0828ef8759
ccrum292 <ccrum292@gmail.com>
2020-03-19 09:43:20 -0400

    skip button now works

--- public/assets/js/write.js
+++ public/assets/js/write.js
@@ -1,12 +1,51 @@
+var lastSentence = [];
+var idOfLastSentence = [];
+
 $(document).ready(function () {
   $("textarea#story").characterCounter();
   $("textarea#entry").characterCounter();
+  $.get("/api/story/return", function(entryData){
+    entryData.forEach(function(val){
+      if(lastSentence.length<10){
+        if(val.Entries.length<3){
+          var allText;
+          var last = {};
+          var storyId = {};
+          if(val.Entries.length>1){
+            allText = val.Entries[val.Entries.length-1].text;
+            var splitText = allText.match(/\(?[^\.\?\!]+[\.!\?]\)?/g);
+            if(splitText.length>1){
+              last.text = splitText[splitText.length - 1];
+            }else{
+              last.text = splitText[0];
+            }
+            storyId.storyId = val.Entries[val.Entries.length-1].StoryId;
+            lastSentence.push(last);
+            idOfLastSentence.push(storyId);
+          }else if(val.Entries.length === 1){
+            last.text = val.Entries[0].text;
+            storyId.storyId = val.Entries[0].StoryId;
+            lastSentence.push(last);
+            idOfLastSentence.push(storyId);
+          }
+        }
+      }
+    });
+  });
 });
 
 // code to change id and last sentence, will need to traverse the dom
-// $("#lastEntry").html("");
-$
-
+var counter = 0;
+$("#nextBtn").on("click", function(event){
+  event.preventDefault();
+  if(counter<10){
+    counter++;
+    var displaySentence = lastSentence[counter].text;
+    var displayId = idOfLastSentence[counter].storyId;
+    $("#lastEntry").html(displaySentence);
+    $("#lastEntry").attr("data-id", displayId);
+  }
+});
 
 $("#createSubmit").on("click", function (event) {
   event.preventDefault();
